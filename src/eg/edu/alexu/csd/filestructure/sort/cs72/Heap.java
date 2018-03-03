d380d99bd0a7a69e6ab739ef4cfbb24252342fb4
youmna_dwidar <youmnadwidar@hotmail.com>
2018-03-03 21:35:30 +0200

    push

--- src/eg/edu/alexu/csd/filestructure/sort/cs72/Heap.java
+++ src/eg/edu/alexu/csd/filestructure/sort/cs72/Heap.java
@@ -112,6 +112,9 @@ public class Heap implements IHeap {
     @Override
     public void build(Collection unordered) {
         ArrayList list = new ArrayList(unordered);
+        if (list.size() == 0) {
+            return;
+        }
         int i = 0;
         Node n = new Node(list.get(i));
         heapArray.add(n);
